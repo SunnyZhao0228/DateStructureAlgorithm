9b53dbf1ecc88a08c85939762d6217a9accf398d
赵庆武 <zhaoqw@brains-info.com>
2022-09-15 10:27:05 +0800

    添加模块

---
+++ AboutMath/src/series/pageinfo.java
@@ -0,0 +1,8 @@
+package series;
+
+/**
+ * 数列相关
+ *
+ * @author zhaoqw
+ * @date 2022/09/15
+ */
---
+++ StringAlgorithm/src/huiwen/packageInfo.java
@@ -0,0 +1,9 @@
+package huiwen;
+
+/**
+ * 字符串算法相关
+ *
+ * @author zhaoqw
+ * @date 2022/09/15
+ */
+
