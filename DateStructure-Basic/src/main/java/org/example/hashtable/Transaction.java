f365aad91fd5fed1ad6209044812aa96c0529ef7
zhaoqw <zhaoqw@syan.com.cn>
2024-01-23 14:31:15 +0800

    fix: add HashCode example

---
+++ DateStructure-Basic/src/main/java/org/example/hashtable/Transaction.java
@@ -0,0 +1,26 @@
+package org.example.hashtable;
+
+import java.util.Date;
+
+/**
+ * HashCode 案例
+ */
+public class Transaction {
+    private final String who;
+    private final Date when;
+    private final double amount;
+
+    public Transaction(String who, Date when, double amount) {
+        this.who = who;
+        this.when = when;
+        this.amount = amount;
+    }
+
+    public int hashCode() {
+        int hash = 17;
+        hash = 31 * hash + who.hashCode();
+        hash = 31 * hash + when.hashCode();
+        hash = 31 * hash + ((Double) amount).hashCode();
+        return hash;
+    }
+}
