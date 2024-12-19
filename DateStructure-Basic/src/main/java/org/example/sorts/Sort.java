8d4bd3988b12836f477cca32daa72bddfccc875e
zhaoqw <zhaoqw@syan.com.cn>
2024-12-19 14:29:14 +0800

    add Sort interface

---
+++ DateStructure-Basic/src/main/java/org/example/sorts/Sort.java
@@ -0,0 +1,8 @@
+package org.example.sorts;
+
+/**
+ * @author zhaoqw
+ * @date 2024/12/19
+ */
+public interface Sort {
+}
---
+++ DateStructure-Basic/src/main/java/org/example/sorts/impl/BubbleSort.java
@@ -0,0 +1,8 @@
+package org.example.sorts.impl;
+
+/**
+ * @author zhaoqw
+ * @date 2024/12/19
+ */
+public class BubbleSort {
+}
